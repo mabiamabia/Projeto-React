059b9cbc530de37e5dcd7bb6a37b3db41b266c4b
mabiamabia <duartecostap@gmail.com>
2023-05-18 20:31:41 -0300

    feat: finalize todo project

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { Bstrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+import {
+  Bstrash,
+  BsBookmarkCheck,
+  BsBookmarkCheckFill,
+  BsTrash,
+} from "react-icons/bs";
 
 const API = "http://localhost:5000";
 
@@ -10,10 +15,75 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.prevenDefault();
-    console.log("Enviou!");
+  //load todos on page load
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+      setTodos(res);
+    };
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => [...prevState, todo]);
+
+    setTitle("");
+    setTime("");
   };
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: { "Content-Type": "application/json" },
+    });
+    setTodos((prevState) =>
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  //  if (title === "" && time === "") {
+  // console.log("não é possivel enviar vazio");
+  //return;
+  //}
 
   return (
     <div className="App">
@@ -29,6 +99,18 @@ function App() {
               type="text"
               name="title"
               placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+            ></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="Tempo estimado (em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
             ></input>
           </div>
           <input type="submit" value="Enviar" />
@@ -37,6 +119,18 @@ function App() {
       <div className="list-todo">
         <h2>Lista de tarefas:</h2>
         {todos.length === 0 && <p>Não há tarefas</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
